c20e64b3f808d3866de8f1c643d1f62f15ce8f17
A-Alonso28 <alexandre.alonso@univ-tlse3.fr>
2024-10-06 21:37:35 +0200

    TP2 terminé

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,19 @@
+package personnages;
+
+public enum Equipement {
+	CASQUE("Casque"), BOUCLIER("Bouclier");
+	
+	private String nom;
+	
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	
+	public String toString() {
+		return nom;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,15 @@ package personnages;
 public class Romain { 
 	private String nom;
 	private int force;
+	private Equipement equipements[];
+	private int nbEquipement;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 		assert isInvariantVerified();
+		this.equipements = new Equipement[2];
+		this.nbEquipement = 0;
 	}
 
 	public String getNom() {
@@ -39,9 +43,38 @@ public class Romain {
 		return this.force > 0;
 	}
 	
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement = nbEquipement + 1;
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.getNom());
+	}
+	
+	private void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if(equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement.getNom());
+			}else {
+				ajouterEquipement(equipement);
+			}
+			break;
+		default:
+			ajouterEquipement(equipement);
+			break;
+		
+		}
+	}
+	
+	
 	public static void main(String [] args) {
-		Romain romain2 = new Romain("romain2", 6);
-		romain2.recevoirCoup(5);
-		romain2.parler("Je teste la méthode parler");
+		Romain minus = new Romain("Minus", 6);
+		
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
