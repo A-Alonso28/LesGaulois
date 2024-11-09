faabaca1a8160b7a47ab489993edadf16ea6190e
A-Alonso28 <alexandre.alonso@univ-tlse3.fr>
2024-11-09 11:42:23 +0100

    TP3 - Mise en place de l'environnement

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,10 +1,10 @@
 package personnages;
 
 public class Gaulois {
-
 	private String nom;
-	private int force;
+	private int force, nb_trophees;
 	private int effetPotion = 1;
+	private Equipement trophees[] = new Equipement[100];
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -19,13 +19,27 @@ public class Gaulois {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 	
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
+	
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+		String texte = "Le gaulois " + nom + " : ";
+		return texte;
 	}
+
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un très grand coup dans la mâchoir de " + romain.getNom());
+//		romain.recevoirCoup(force / 3 * effetPotion);
+//	}
 	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un très grand coup dans la mâchoir de " + romain.getNom());
-		romain.recevoirCoup(force / 3 * effetPotion);
+		System.out.println(nom + " envoie un grand coup dans lamâchoire de " + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) * effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++, nb_trophees++) {
+			this.trophees[nb_trophees] = trophees[i];
+		}
+		return;
 	}
 
 	@Override
